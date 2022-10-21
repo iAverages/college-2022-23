56885912c94c50469a62c0da6f4359ae640d4c41
iAverages <31937542+iAverages@users.noreply.github.com>
2022-10-21 13:55:37 +0100

    Add setup.bat
    Does not work how I want it to atm, will fix later

---
+++ setup.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set path=%path%;%cd%/bin/node-v16.18.0-win-x64
+
+echo "Added Node v16 to path for terminal session."
+echo ""
+echo "You will need to run this setup script for every terminal session where you want to use node"
