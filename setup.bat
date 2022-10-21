da9ebdde58257d853dbeb324af7a5c5e9688072c
iAverages <31937542+iAverages@users.noreply.github.com>
2022-10-21 15:56:06 +0100

    remove .npmrc will use global config

--- setup.bat
+++ setup.bat
@@ -1,6 +1,10 @@
 @echo off
 
-set path=%path%;%cd%/bin/node-v16.18.0-win-x64
+@REM This does not work atm dont use
+
+set path=%path%;%cd%\bin\node-v16.18.0-win-x64
+NODE_TLS_REJECT_UNAUTHORIZED=0 npm i
+npm config set strict-ssl false 
 
 echo "Added Node v16 to path for terminal session."
 echo ""
