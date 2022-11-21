2c104f0270d17afa10a6e76484cda163f58011fa
iAvearges <31937542+iAverages@users.noreply.github.com>
2022-11-21 14:22:42 +0000

    OOP cats example

---
+++ projects/examples/Cats/Cats/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Cats
+{
+    internal class Program
+    {
+
+        ///<summary> with a large program, I might instantiate a ProgramDriver class (or words to that effect) here. This is something I would create 
+        /// with a large program, I might instantiate a ProgramDriver class (or words to that effect) here. This is something I would create to act as the 
+        /// main controller of my program (again to get away from the static void main!)
+        /// </summary>
+        /// <param name="args">test</param>
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat("Tabby","Multicoloured",false);
+            Console.WriteLine("Your cat is a " + kitty.CatType + " and is " + kitty.CatColour);
+            kitty.makeNoise();
+            Console.ReadLine();
+        }
+    }
+}
