68daef1144e3bef9c1656f1f8f8c784920d9ab14
iAverages <me@danielraybone.com>
2022-12-07 12:34:38 +0000

    Change config defaults

--- projects/os-mock-1/src/pages/_app.tsx
+++ projects/os-mock-1/src/pages/_app.tsx
@@ -6,15 +6,12 @@ import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 };
 
 export default trpc.withTRPC(MyApp);
