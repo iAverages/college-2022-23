1adf48fefe496870b3afdd5e6fe459278174a15b
iAverages <me@danielraybone.com>
2022-12-14 13:54:17 +0000

    Fix breadcrumb separator

--- projects/os-mock-1/src/components/layout.tsx
+++ projects/os-mock-1/src/components/layout.tsx
@@ -2,29 +2,28 @@ import { Breadcrumb, BreadcrumbItem as ChakraBreadcrumbItem, BreadcrumbLink, Con
 import useBreadcrumbsStore, { BreadcrumbItem } from "@hooks/useBreadcrumbsStore";
 import { capital } from "@utils/stringFormat";
 import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { ReactNode, useEffect } from "react";
+import { type ReactNode, useEffect } from "react";
 import Navigation from "./nav";
 
 type LayoutProps = {
     children: ReactNode | ReactNode[];
 };
 
-const BreadcrumbItem: React.FC<BreadcrumbItem> = ({ href, name }) => (
-    <ChakraBreadcrumbItem>
+const BreadcrumbItem: React.FC<BreadcrumbItem> = ({ href, name, ...props }) => (
+    <ChakraBreadcrumbItem {...props}>
         <BreadcrumbLink href={href}>{capital(name)}</BreadcrumbLink>
     </ChakraBreadcrumbItem>
 );
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     const breadcrumbs = useBreadcrumbsStore((state) => state.items);
-    const session = useSession()
+    const session = useSession();
 
     useEffect(() => {
         if (session.status === "unauthenticated") {
-            signIn("github")
+            signIn("github");
         }
-    },[])
+    }, [session.status]);
 
     return (
         <div className="flex flex-col">
